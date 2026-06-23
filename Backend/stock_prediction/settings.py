84845c33d344ad4f3cfe94e56ebc06dae78ac8de
muhindi001 <muhindijohn128@gmail.com>
2026-06-23 18:54:04 +0300

    Update project

--- Backend/stock_prediction/settings.py
+++ Backend/stock_prediction/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'OnlineMarket',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '123456789',
         'HOST': 'localhost',  
         'PORT': '5432',       
     }
